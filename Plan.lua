d23a322cbe58e05eb89e91af69a17612a94c7e4d
A. Lemme <andre.lemme@xamla.com>
2016-05-11 15:42:49 +0200

    added plotting functionallity to class

--- Plan.lua
+++ Plan.lua
@@ -3,7 +3,7 @@ local torch = require 'torch'
 local ros = require 'ros'
 local moveit = require 'moveit.env'
 local utils = require 'moveit.utils'
-
+local gnuplot = require 'gnuplot'
 local Plan = torch.class('moveit.Plan', moveit)
 
 local f
@@ -15,7 +15,10 @@ function init()
     "release",
     "getStartStateMsg",
     "getTrajectoryMsg",
-    "getPlanningTime"
+    "getPlanningTime",
+    "plot",
+    "convertTrajectoyMsgToTable",
+    "convertStartStateMsgToTensor"
   }
   f = utils.create_method_table("moveit_Plan_", Plan_method_names)
 end
@@ -54,4 +57,102 @@ end
 
 function Plan:getPlanningTime()
   return f.getPlannigTime(self.o)
+end
+
+function Plan:convertTrajectoyMsgToTable(trajectory_msg) -- expect a Utils object 
+    local positions = {}
+  local velocities = {}
+  local accelerations = {}
+  local efforts = {}
+  local points = trajectory_msg.joint_trajectory.points
+
+  for i=1,#points do
+    positions[i] = points[i].positions
+    velocities[i] = points[i].velocities
+    accelerations[i] = points[i].accelerations
+    efforts[i] = points[i].effort
+  end
+  return positions,velocities,accelerations,efforts
+end
+
+function Plan:convertStartStateMsgToTensor(start_state) -- expect a Utils object 
+  position = start_state.joint_state.position
+  velocity = start_state.joint_state.velocity
+  labels = start_state.joint_state.names
+  return position, velocity, labels
+end
+
+
+local function plot6DTrajectory(trajectory)
+    if trajectory[1]:nDimension()==0 then 
+      return false 
+    end
+    print(trajectory[1])
+    local history_size = #trajectory   
+ 
+    local q1={}
+    local q2={}
+    local q3={}
+    local q4={}
+    local q5={}
+    local q6={}
+    for i=1,history_size do
+      q1[#q1+1] = trajectory[i][1]
+      q2[#q2+1] = trajectory[i][2]
+      q3[#q3+1] = trajectory[i][3]
+      q4[#q4+1] = trajectory[i][4]
+      q5[#q5+1] = trajectory[i][5]
+      q6[#q6+1] = trajectory[i][6]
+    end
+    local q1_tensor = torch.Tensor(q1)
+    local q2_tensor = torch.Tensor(q2)
+    local q3_tensor = torch.Tensor(q3)
+    local q4_tensor = torch.Tensor(q4)
+    local q5_tensor = torch.Tensor(q5)
+    local q6_tensor = torch.Tensor(q6)
+    gnuplot.plot({'q1',q1_tensor}, {'q2',q2_tensor}, {'q3',q3_tensor},{'q4',q4_tensor},{'q5',q5_tensor},{'q6',q6_tensor})
+  --gnuplot.axis{0, history_size, -100, 100}
+    gnuplot.grid(true)
+    return true
+end
+
+function Plan:plot(type)
+  local msg
+  msg= self:getTrajectoryMsg()--Position
+  local positions,velocities,accelerations,efforts= self:convertTrajectoyMsgToTable(msg)
+  
+  gnuplot.figure(type)
+
+  if type == 1 then
+    if plot6DTrajectory(positions)then 
+    gnuplot.title('Trajectory position Data')
+    end
+  elseif type == 2 then
+    if plot6DTrajectory(velocities) then
+    gnuplot.title('Trajectory velocity Data')
+    end
+  elseif type == 3 then
+    if plot6DTrajectory(accelerations) then
+      gnuplot.title('Trajectory accelaration Data')
+    end
+  elseif type ==4 then
+    if  velocities[1]:nDimension()==0 then 
+      return false 
+    end
+    history_size = #velocities
+    local q = {}
+    for i=1,#velocities do
+      q[#q+1] = torch.norm(velocities[i])
+    end
+    local q_tensor = torch.Tensor(q)
+    gnuplot.plot({'speed',q_tensor})
+    --gnuplot.axis{0, history_size, -100, 100}
+    gnuplot.grid(true)
+    gnuplot.title('Trajectory speed Data')
+  else
+    print("plot type not yet implemented")
+  end
+  
+
+  return true
 end
