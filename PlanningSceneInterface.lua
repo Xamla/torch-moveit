a5db3a878b3b5545b957f74c84624fc3f955e828
A. Lemme <andre.lemme@xamla.com>
2016-06-16 10:38:12 +0200

    added docu

--- PlanningSceneInterface.lua
+++ PlanningSceneInterface.lua
@@ -3,6 +3,11 @@ local torch = require 'torch'
 local moveit = require 'moveit.env'
 local utils = require 'moveit.utils'
 
+----------------------
+-- LUA wrapper for moveit PlanningSceneInterface environment
+-- dependency to tourch.ros
+-- module: MoveGroup
+--
 local PlanningSceneInterface = torch.class('moveit.PlanningSceneInterface', moveit)
 
 local f
@@ -30,6 +35,13 @@ function PlanningSceneInterface:cdata()
   return self.o
 end
 
+---function addBox
+--Add a solit box as a collision object into the planning scene
+--@tparam string
+--@tparam number
+--@tparam number
+--@tparam number
+--@tparam tf.Transform
 function PlanningSceneInterface:addBox(id, w, h, t, pose)
   local obj = moveit.CollisionObject()
   obj:setId(id)
@@ -38,6 +50,11 @@ function PlanningSceneInterface:addBox(id, w, h, t, pose)
   self:addCollisionObject(obj)
 end
 
+---function addSphere
+--Add a solit sphere as a collision object into the planning scene
+--@tparam string
+--@tparam number
+--@tparam tf.Transform
 function PlanningSceneInterface:addSphere(id, radius, pose)
   local obj = moveit.CollisionObject()
   obj:setId(id)
@@ -46,7 +63,15 @@ function PlanningSceneInterface:addSphere(id, radius, pose)
   self:addCollisionObject(obj)
 end
 
--- ax + by + cz + d = 0
+---function addSphere
+--Add a solit sphere as a collision object into the planning scene
+--ax + by + cz + d = 0
+--@tparam string
+--@tparam number
+--@tparam number
+--@tparam number
+--@tparam number
+--@tparam tf.Transform
 function PlanningSceneInterface:addPlane(id, a, b, c, d, pose)
   local obj = moveit.CollisionObject()
   obj:setId(id)
@@ -54,21 +79,43 @@ function PlanningSceneInterface:addPlane(id, a, b, c, d, pose)
   self:addCollisionObject(obj)
 end
 
+---function addCollisionObject
+--Add a collision object
+--@tparam moveit.CollisionObject()
 function PlanningSceneInterface:addCollisionObject(collision_object)
   f.addCollisionObject(self.o, collision_object:cdata())
 end
 
+---function addCollisionObject
+--Remove a collision object
+--@tparam table object ids as strings
 function PlanningSceneInterface:removeCollisionObjects(object_ids)
   local ids = std.StringVector(object_ids)
   f.removeCollisionObjects(self.o, ids:cdata())
 end
 
+---function getKnownObjectNames
+--Get the names of all known objects in the world. If with_type is set to true, only return objects that have a known type.
+--@tparam[opt] bool
+--@treturn std.StringVector()
 function PlanningSceneInterface:getKnownObjectNames(with_type)
   local result = std.StringVector()
   f.getKnownObjectNames(self.o, with_type or false, result:cdata())
   return result
 end
 
+---function getKnownObjectNamesInROI
+--Get the names of known objects in the world that are located within a bounding region (specified in the frame reported by getPlanningFrame()).
+--If with_type is set to true, only return objects that have a known type.
+--@tparam number
+--@tparam number
+--@tparam number
+--@tparam number
+--@tparam number
+--@tparam number
+--@tparam[opt=false] bool
+--@tparam[opt] ?std.StringVector()|list
+--@treturn std.StringVector()
 function PlanningSceneInterface:getKnownObjectNamesInROI(minx, miny, minz, maxx, maxy, maxz, with_type, types)
   if types and not torch.isTypeOf(types, std.StringVector) then
     types = std.StringVector(types)
@@ -78,6 +125,11 @@ function PlanningSceneInterface:getKnownObjectNamesInROI(minx, miny, minz, maxx,
   return result
 end
 
+---function getObjectPoses
+--Get poses of objects specified in object_ids
+--@tparam[opt] ?std.StringVector()|list
+--@treturn bool
+--@treturn torch.DoubleTensor()
 function PlanningSceneInterface:getObjectPoses(object_ids)
   if not torch.isTypeOf(object_ids, std.StringVector) then
     object_ids = std.StringVector(object_ids)
